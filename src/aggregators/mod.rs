7112cebf4a306519179b7a63b893b348ae5897cb
ultraelectronica <fyketonel@gmail.com>
2026-06-16 01:15:49 +0800

    Export additional command counting and ranking functions

--- src/aggregators/mod.rs
+++ src/aggregators/mod.rs
@@ -4,6 +4,6 @@ pub mod command_counter;
 pub mod command_filter;
 pub mod command_ranker;
 
-pub use command_counter::count_commands;
+pub use command_counter::{count_commands, count_commands_at_depth};
 pub use command_filter::{filter_by_min_frequency, filter_commands, top_n};
-pub use command_ranker::rank_commands;
+pub use command_ranker::{rank_commands, rank_commands_ascending};
